4eda917e6ae6f827d4b5cc8d54bee213422046ca
DeWin Bebebtos <DeWin Bebebtos>
2017-03-13 15:36:51 +0100

    'update'

--- todo.js
+++ todo.js
@@ -1,7 +1,2 @@
-// webpack config pour les ressources = ok
-// webpack config du build pour css et js =
 // promise puis emitter pour le loader =
 // datgui pour la vue =
-// les mixins pour les flexbox = ok
-
-//add html webpack plugin =
